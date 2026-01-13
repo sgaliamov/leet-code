25f65845bd3d7b125325a6ed895ca98955926f46
s. galiamov <6alyamov@gmail.com>
2026-01-13 22:15:54 +0100

    remove_element done

--- tests/remove-element.rs
+++ tests/remove-element.rs
@@ -14,7 +14,7 @@ pub fn remove_element_dummy(nums: &mut Vec<i32>, val: i32) -> i32 {
     nums.len() as i32
 }
 
-// not mine
+// from leet code
 pub fn remove_element_perfect(nums: &mut Vec<i32>, val: i32) -> i32 {
     nums.retain(|&num| num != val);
     nums.len() as i32
@@ -34,7 +34,7 @@ pub fn remove_element_swap_remove(nums: &mut Vec<i32>, val: i32) -> i32 {
 }
 
 // my first ugly solution
-pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+pub fn remove_element_ugly(nums: &mut Vec<i32>, val: i32) -> i32 {
     let mut j = nums.len();
     let mut i = 0;
     let mut s = 0;
@@ -66,6 +66,23 @@ pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
     (nums.len() - s) as i32
 }
 
+// the right solution, not mine.
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < nums.len() {
+        if nums[i] != val {
+            nums[j] = nums[i];
+            j += 1;
+        }
+
+        i += 1;
+    }
+
+    j as i32
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -76,6 +93,12 @@ mod tests {
     fn test() {
         run_test(remove_element);
     }
+
+    #[test]
+    fn test_ugly() {
+        run_test(remove_element_ugly);
+    }
+
     #[test]
     fn test_dummy() {
         run_test(remove_element_dummy);
@@ -94,6 +117,7 @@ mod tests {
     fn run_test(target: fn(&mut Vec<i32>, i32) -> i32) {
         vec![
             (vec![3, 2, 2, 3], 3, vec![2, 2]),
+            (vec![2, 2, 3], 3, vec![2, 2]),
             (vec![2, 3], 3, vec![2]),
             (vec![3, 3], 3, vec![]),
             (vec![0, 1, 2, 2, 3, 0, 4, 2], 2, vec![0, 0, 1, 3, 4]),
@@ -107,7 +131,7 @@ mod tests {
         .for_each(|(mut nums, val, expected)| {
             let actual = target(&mut nums, val) as usize;
             let nums = nums.into_iter().take(expected.len()).sorted().collect_vec();
-            let name = format!("{nums:?} | {expected:?} | {actual}");
+            let name = format!("{nums:?} | {val} | {expected:?} | {actual}");
 
             assert_that!(nums).named(&name).is_equal_to(&expected);
             assert_that!(actual)
