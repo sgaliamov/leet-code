b315546d8a0c84cded0380534e17c6f899998880
s. galiamov <6alyamov@gmail.com>
2026-01-13 11:21:45 +0100

    done

--- tests/merge-sorted-array.rs
+++ tests/merge-sorted-array.rs
@@ -1,29 +1,25 @@
-/*
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
-*/
+//! You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//! Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//! The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+//! To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored.
+//! nums2 has a length of n.
 
 use spectral::assert_that;
 
 /// https://leetcode.com/problems/merge-sorted-array
 pub fn merge(nums1: &mut Vec<i32>, mut m: i32, nums2: &mut Vec<i32>, mut n: i32) {
-    use std::mem;
-    assert!(nums1.iter().take(m as usize).is_sorted(), "{:?}", nums1);
-    assert!(nums2.is_sorted());
-
     if n == 0 {
         return;
     }
 
     if m == 0 {
-        mem::swap(nums1, nums2);
+        std::mem::swap(nums1, nums2);
         return;
     }
 
-    let mut i = nums1.len() - 1;
+    let mut i = (m + n - 1) as usize;
 
-    while i != 0 {
+    while m != 0 && n != 0 {
         let mi = (m - 1) as usize;
         let ni = (n - 1) as usize;
 
@@ -35,31 +31,18 @@ pub fn merge(nums1: &mut Vec<i32>, mut m: i32, nums2: &mut Vec<i32>, mut n: i32)
             m -= 1;
             i -= 1;
             nums1[i] = nums2[ni];
-            n -= 1;
-        } else if nums1[mi] < nums2[ni] {
+            n = ni as i32;
+        } else {
             nums1[i] = nums2[ni];
             n -= 1;
-        }
-
-        if n == 0 {
-            break;
-        }
-
-        if m == 0 {
-            for j in 0..n {
-                let j = j as usize;
-                nums1[j] = nums2[j]
-            }
-            break;
         }
 
         i -= 1;
     }
 
-    // we cannot continue to decrease `m` and `n` as they will become negative,
-    // therefore, we have to check the last element directly:
-    if nums1[0] > nums2[0] {
-        nums1[0] = nums2[0];
+    for j in 0..n {
+        let j = j as usize;
+        nums1[j] = nums2[j]
     }
 }
 
