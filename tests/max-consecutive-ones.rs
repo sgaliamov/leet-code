b859ca7cd0df3ce271fe1217b2ce77d6502dcf69
s. galiamov <6alyamov@gmail.com>
2026-01-15 19:43:30 +0100

    find_max_consecutive_ones

--- tests/max-consecutive-ones.rs
+++ tests/max-consecutive-ones.rs
@@ -3,7 +3,31 @@
 //! Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
 pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
-    0
+    let mut max = 0;
+    let mut c = 0;
+
+    for i in 0..nums.len() {
+        let x = nums[i];
+
+        if x == 1 {
+            c += 1;
+        } else {
+            if c > max {
+                max = c;
+
+                if nums.len() - i <= max {
+                    break;
+                }
+            }
+            c = 0;
+        }
+    }
+
+    if c > max {
+        max = c;
+    }
+
+    max as i32
 }
 
 #[cfg(test)]
@@ -17,13 +41,16 @@ mod tests {
     }
 
     fn run_test(target: fn(Vec<i32>) -> i32) {
-        vec![(vec![1,1,0,1,1,1], 3)]
-            .into_iter()
-            .for_each(|(nums, expected)| {
-                let actual = target(nums);
-                let name = format!("{expected:?}");
+        vec![
+            (vec![1, 1, 0, 1, 1, 1], 3),
+            (vec![1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1], 4),
+        ]
+        .into_iter()
+        .for_each(|(nums, expected)| {
+            let actual = target(nums);
+            let name = format!("{expected:?}");
 
-                assert_that!(actual).named(&name).is_equal_to(expected);
-            });
+            assert_that!(actual).named(&name).is_equal_to(expected);
+        });
     }
 }
