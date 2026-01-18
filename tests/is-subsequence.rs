051656e257a39a16d4b543be6b38e09427b7b949
s. galiamov <6alyamov@gmail.com>
2026-01-18 15:42:18 +0100

    done

--- tests/is-subsequence.rs
+++ tests/is-subsequence.rs
@@ -4,21 +4,26 @@
 //! A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 pub fn is_subsequence(s: String, t: String) -> bool {
-    let mut i = 0;
-    let mut j = 0;
-    let s = s.as_bytes();
-    let t = t.as_bytes();
-
-    while j < t.len() && i < s.len() {
-        if s[i] == t[j] {
-            i += 1;
-            j += 1;
-        } else {
-            j += 1;
-        }
+    if s.is_empty(){
+        return true;
     }
 
-    i == s.len()
+    let s = s.as_bytes();
+    let t = t.as_bytes();
+    let (mut i, mut j, m, n) = (0, 0, s.len(), t.len());
+
+    while j < n && i < m {
+        if s[i] == t[j] {
+            i += 1;
+            if i == m {
+                return true;
+            }
+        }
+
+        j += 1;
+    }
+
+    false
 }
 
 #[cfg(test)]
