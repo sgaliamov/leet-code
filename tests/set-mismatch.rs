9262cff1b0b046b5794bff2c12a5e23a98972954
s. galiamov <6alyamov@gmail.com>
2026-01-18 19:57:14 +0100

    find_error_nums_2

--- tests/set-mismatch.rs
+++ tests/set-mismatch.rs
@@ -4,7 +4,8 @@
 //! You are given an integer array nums representing the data status of this set after the error.
 //! Find the number that occurs twice and the number that is missing and return them in the form of an array.
 
-pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+/// slow
+pub fn find_error_nums_1(nums: Vec<i32>) -> Vec<i32> {
     let mut d = 0;
     let mut set = std::collections::HashSet::<i32>::new();
 
@@ -24,14 +25,42 @@ pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
     vec![]
 }
 
+/// fast
+pub fn find_error_nums_2(nums: Vec<i32>) -> Vec<i32> {
+    let mut d = 0;
+    let mut set = vec![false; nums.len() + 1];
+
+    for &val in &nums {
+        let i = val as usize;
+        if set[i] {
+            d = i;
+        }
+        set[i] = true;
+    }
+
+    #[allow(clippy::needless_range_loop)]
+    for val in 1..=nums.len() {
+        if !set[val] {
+            return vec![d as i32, val as i32];
+        }
+    }
+
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use spectral::prelude::*;
 
     #[test]
-    fn test() {
-        run_test(find_error_nums);
+    fn test_1() {
+        run_test(find_error_nums_1);
+    }
+
+    #[test]
+    fn test_2() {
+        run_test(find_error_nums_2);
     }
 
     fn run_test(target: fn(Vec<i32>) -> Vec<i32>) {
@@ -47,7 +76,7 @@ mod tests {
         ]
         .into_iter()
         .for_each(|(nums, expected)| {
-            let name = format!("{nums:?}->{expected:?}");
+            let name = format!("{nums:?} → {expected:?}");
             let actual = target(nums);
 
             assert_that!(actual).named(&name).is_equal_to(&expected);
