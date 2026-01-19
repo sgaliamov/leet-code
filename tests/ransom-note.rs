082095a781f08f6469577c5466b7a76a4d525b1c
s. galiamov <6alyamov@gmail.com>
2026-01-19 23:02:21 +0100

    start

---
+++ tests/ransom-note.rs
@@ -0,0 +1,29 @@
+//! https://leetcode.com/problems/ransom-note
+//!
+//! Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+//! Each letter in magazine can only be used once in ransomNote.
+
+pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spectral::prelude::*;
+
+    #[test]
+    fn test() {
+        run_test(can_construct);
+    }
+
+    fn run_test(target: fn(String, String) -> bool) {
+        vec![("a", "b", false), ("aa", "ab", false), ("aa", "aab", true)]
+            .into_iter()
+            .for_each(|(ransom_note, magazine, expected)| {
+                let name = format!("{ransom_note} {magazine}");
+                let actual = target(ransom_note.to_string(), magazine.to_string());
+                assert_that!(actual).named(&name).is_equal_to(expected);
+            });
+    }
+}
