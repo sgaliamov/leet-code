633535f0cd53272723bd0b685ac93906692b36b3
s. galiamov <6alyamov@gmail.com>
2026-01-20 23:05:07 +0100

    can_construct_3

--- tests/ransom-note.rs
+++ tests/ransom-note.rs
@@ -3,6 +3,8 @@
 //! Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 //! Each letter in magazine can only be used once in ransomNote.
 
+use std::f32::consts::E;
+
 /// slow
 pub fn can_construct_1(ransom_note: String, magazine: String) -> bool {
     use itertools::Itertools;
@@ -56,6 +58,51 @@ pub fn can_construct_2(ransom_note: String, magazine: String) -> bool {
     true
 }
 
+///
+pub fn can_construct_3(ransom_note: String, magazine: String) -> bool {
+    use std::collections::HashMap;
+    let mut letters: HashMap<_, _, _> =
+        HashMap::with_capacity_and_hasher(28, std::hash::RandomState::default());
+    let mut mi = 0;
+    let magazine = magazine.as_bytes();
+
+    for l in ransom_note.as_bytes() {
+        if let Some(fi) = find(l, mi, magazine, &mut letters) {
+            mi = fi;
+            letters.entry(*l).and_modify(|e| *e -= 1);
+        } else {
+            return false;
+        };
+    }
+
+    true
+}
+
+fn find(
+    letter: &u8,
+    mut mi: usize,
+    magazine: &[u8],
+    letters: &mut std::collections::HashMap<u8, i32>,
+) -> Option<usize> {
+    if let Some(e) = letters.get(letter)
+        && e != &0
+    {
+        return Some(mi);
+    }
+
+    while mi < magazine.len() {
+        let ml = magazine[mi];
+        letters.entry(ml).and_modify(|e| *e += 1).or_insert(1);
+        mi += 1;
+
+        if &ml == letter {
+            return Some(mi);
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,13 +118,22 @@ mod tests {
         run_test(can_construct_2);
     }
 
+    #[test]
+    fn test_3() {
+        run_test(can_construct_3);
+    }
+
     fn run_test(target: fn(String, String) -> bool) {
-        vec![("a", "b", false), ("aa", "ab", false), ("aa", "aab", true)]
-            .into_iter()
-            .for_each(|(ransom_note, magazine, expected)| {
-                let name = format!("{ransom_note} {magazine}");
-                let actual = target(ransom_note.to_string(), magazine.to_string());
-                assert_that!(actual).named(&name).is_equal_to(expected);
-            });
+        vec![
+            // ("a", "b", false), //
+            ("aa", "ab", false),
+            ("aa", "aab", true),
+        ]
+        .into_iter()
+        .for_each(|(ransom_note, magazine, expected)| {
+            let name = format!("{ransom_note} {magazine}");
+            let actual = target(ransom_note.to_string(), magazine.to_string());
+            assert_that!(actual).named(&name).is_equal_to(expected);
+        });
     }
 }
