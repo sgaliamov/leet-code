3cbcdbbf54bc012f91056a944135dd0d349df871
s. galiamov <6alyamov@gmail.com>
2026-01-21 21:13:40 +0100

    is_isomorphic_1

--- tests/isomorphic-strings.rs
+++ tests/isomorphic-strings.rs
@@ -2,10 +2,37 @@
 //!
 //! Given two strings s and t, determine if they are isomorphic.
 //! Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-//! All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+//! All occurrences of a character must be replaced with another character while preserving the order of characters.
+//! No two characters may map to the same character, but a character may map to itself.
 
-pub fn is_isomorphic_1(r: String, t: String) -> bool {
-    true
+// first attempt.
+// beats 100%, but failed on memory.
+pub fn is_isomorphic_1(s: String, t: String) -> bool {
+    fn index(value: &str) -> Vec<Vec<u16>> {
+        const NONE: u16 = u16::MAX;
+        let mut pos_s = [NONE; 256]; // first positions for each character
+        let mut ind_s: Vec<Vec<u16>> = vec![Vec::new(); 256]; // relative index
+
+        for (i, l) in value.bytes().map(|l| l as usize).enumerate() {
+            if pos_s[l] == NONE {
+                pos_s[l] = i as u16
+            }
+
+            let pos = pos_s[l] as usize;
+            ind_s[pos].push(i as u16);
+        }
+
+        ind_s
+    }
+
+    if s.len() != t.len() {
+        return false;
+    }
+
+    let ind_s = index(&s);
+    let ind_t = index(&t);
+
+    ind_s.eq(&ind_t)
 }
 
 #[cfg(test)]
@@ -21,9 +48,11 @@ mod tests {
 
     fn run_test(target: fn(String, String) -> bool) {
         vec![
+            ("paper", "title", true),
             ("egg", "add", true),
             ("foo", "bar", false),
-            ("paper", "title", true),
+            ("13", "42", true),
+            ("!!#", "%%!", true),
         ]
         .into_iter()
         .for_each(|(s, t, expected)| {
