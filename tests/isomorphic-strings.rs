9f3c80c85757b7e98243463e32aac6552cef52f3
s. galiamov <6alyamov@gmail.com>
2026-01-21 21:53:44 +0100

    is_isomorphic_2

--- tests/isomorphic-strings.rs
+++ tests/isomorphic-strings.rs
@@ -10,19 +10,19 @@
 pub fn is_isomorphic_1(s: String, t: String) -> bool {
     fn index(value: &str) -> Vec<Vec<u16>> {
         const NONE: u16 = u16::MAX;
-        let mut pos_s = [NONE; 256]; // first positions for each character
-        let mut ind_s: Vec<Vec<u16>> = vec![Vec::new(); 256]; // relative index
+        let mut positions = [NONE; 256]; // first positions for each character
+        let mut index: Vec<Vec<u16>> = vec![Vec::new(); 256]; // relative index
 
         for (i, l) in value.bytes().map(|l| l as usize).enumerate() {
-            if pos_s[l] == NONE {
-                pos_s[l] = i as u16
+            if positions[l] == NONE {
+                positions[l] = i as u16
             }
 
-            let pos = pos_s[l] as usize;
-            ind_s[pos].push(i as u16);
+            let pos = positions[l] as usize;
+            index[pos].push(i as u16);
         }
 
-        ind_s
+        index
     }
 
     if s.len() != t.len() {
@@ -35,6 +35,41 @@ pub fn is_isomorphic_1(s: String, t: String) -> bool {
     ind_s.eq(&ind_t)
 }
 
+// better memory (85%).
+pub fn is_isomorphic_2(s: String, t: String) -> bool {
+    if s.len() != t.len() {
+        return false;
+    }
+
+    const NONE: u16 = u16::MAX;
+    let mut pos_s = [NONE; 256]; // first positions for each character
+    let mut pos_t = [NONE; 256];
+    let s = s.as_bytes();
+    let t = t.as_bytes();
+
+    for i in 0..s.len() {
+        let cs = s[i] as usize;
+        let ct = t[i] as usize;
+
+        if pos_s[cs] == NONE {
+            pos_s[cs] = i as u16
+        }
+
+        if pos_t[ct] == NONE {
+            pos_t[ct] = i as u16
+        }
+
+        let ps = pos_s[cs] as usize;
+        let pt = pos_t[ct] as usize;
+
+        if ps != pt {
+            return false;
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,6 +79,7 @@ mod tests {
     solution_tests!(
         run_test:
         is_isomorphic_1,
+        is_isomorphic_2,
     );
 
     fn run_test(target: fn(String, String) -> bool) {
