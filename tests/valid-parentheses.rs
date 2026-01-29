a1fe3beaedaa3aba8412cb38933d196455083c6f
s. galiamov <6alyamov@gmail.com>
2026-01-29 21:11:56 +0100

    is_valid_3

--- tests/valid-parentheses.rs
+++ tests/valid-parentheses.rs
@@ -7,8 +7,10 @@
 //! - Every close bracket has a corresponding open bracket of the same type.
 //!
 //! Constraints:
-//! 1 <= s.length <= 104
+//! 1 <= s.length <= 10^4
 //! s consists of parentheses only '()[]{}'.
+
+use std::{alloc::Layout, mem};
 
 // 100-0/78-2.19
 pub fn is_valid_1(s: String) -> bool {
@@ -39,6 +41,71 @@ pub fn is_valid_1(s: String) -> bool {
     stack.is_empty()
 }
 
+pub fn is_valid_2(s: String) -> bool {
+    // if s.len() % 2 != 0 {
+    //     return false;
+    // }
+
+    // let mut stack = vec![0_u64; (s.len() / 2 / 64).max(1)];
+    // let mut cursor = 0;
+    // const STEP: usize = 8;
+    // for c in s.into_bytes() {
+    //     let tbit = match c {
+    //         b'(' => 0x010,
+    //         b')' => 0x011,
+    //         b'[' => 0x100,
+    //         b']' => 0x101,
+    //         b'{' => 0x110,
+    //         b'}' => 0x111,
+    //         _ => unreachable!(),
+    //     };
+    //     let bucket = cursor / 64;
+    //     let stack = stack[bucket];
+    //     let i = cursor % STEP;
+    //     stack |= 1 << i;
+    // }
+
+    false
+}
+
+// 100/99/2.06
+pub fn is_valid_3(s: String) -> bool {
+    let mut k: i32 = -1;
+    let mut bytes = s.into_bytes();
+
+    for i in 0..bytes.len() {
+        let c = bytes[i];
+        match c {
+            b'(' | b'[' | b'{' => {
+                k += 1;
+                bytes[k as usize] = c;
+            }
+
+            _ => {
+                if k == -1 {
+                    return false;
+                }
+
+                let l = bytes[k as usize];
+                k -= 1;
+
+                let r = match c {
+                    b')' => b'(',
+                    b']' => b'[',
+                    b'}' => b'{',
+                    _ => unreachable!(),
+                };
+
+                if r != l {
+                    return false;
+                }
+            }
+        }
+    }
+
+    k == -1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,12 +115,13 @@ mod tests {
     solution_tests!(
         run_test:
         is_valid_1,
+        is_valid_3,
     );
 
     fn run_test(target: fn(String) -> bool) {
         vec![
-            ("(", false),
             ("()[]{}", true),
+            ("(", false),
             ("()", true),
             ("]", false),
             ("(]", false),
