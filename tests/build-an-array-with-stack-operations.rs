a0d3f77d81fdbae379e11338af9c0e90b6cf1a9e
s. galiamov <6alyamov@gmail.com>
2026-01-30 22:11:25 +0100

    start

---
+++ tests/build-an-array-with-stack-operations.rs
@@ -0,0 +1,63 @@
+//! https://leetcode.com/problems/build-an-array-with-stack-operations/
+//!
+//! You are given an integer array target and an integer n.
+//!
+//! You have an empty stack with the two following operations:
+//! - "Push": pushes an integer to the top of the stack.
+//! - "Pop": removes the integer on the top of the stack.
+//!
+//! You also have a stream of the integers in the range [1, n].
+//!
+//! Use the two stack operations to make the numbers in the stack (from the bottom to the top)
+//! equal to target. You should follow the following rules:
+//! - If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
+//! - If the stack is not empty, pop the integer at the top of the stack.
+//! - If, at any moment, the elements in the stack (from the bottom to the top) are equal to target,
+//!   do not read new integers from the stream and do not do more operations on the stack.
+//!
+//! Return the stack operations needed to build target following the mentioned rules.
+//! If there are multiple valid answers, return any of them.
+//!
+//! Constraints:
+//! - 1 <= target.length <= 100
+//! - 1 <= n <= 100
+//! - 1 <= target[i] <= n
+//! - target is strictly increasing
+
+pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spectral::prelude::*;
+
+    #[test]
+    fn build_array_test() {
+        let cases = vec![
+            (
+                vec![1, 3],
+                3,
+                vec!["Push", "Push", "Pop", "Push"],
+            ), // Read 1, push; Read 2, push & pop; Read 3, push
+            (
+                vec![1, 2, 3],
+                3,
+                vec!["Push", "Push", "Push"],
+            ), // All sequential, just push
+            (
+                vec![1, 2],
+                4,
+                vec!["Push", "Push"],
+            ), // Stop after building target
+        ];
+
+        cases.into_iter().for_each(|(target, n, expected)| {
+            let name = format!("build_array({:?}, {})", target, n);
+            let expected: Vec<String> = expected.into_iter().map(String::from).collect();
+            let actual = build_array(target, n);
+            assert_that!(actual).named(&name).is_equal_to(expected);
+        });
+    }
+}
