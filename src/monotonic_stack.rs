bce0a1a75b8eb5922d2b59395b12f69fa54f3377
s. galiamov <6alyamov@gmail.com>
2026-02-03 18:39:20 +0100

    MonotonicDescStack

---
+++ src/monotonic_stack.rs
@@ -0,0 +1,68 @@
+/// Decreasing monotonic stack
+pub struct MonotonicDescStack<T> {
+    stack: Vec<T>,
+}
+
+impl<T: PartialOrd> MonotonicDescStack<T> {
+    #[inline]
+    pub fn push(&mut self, val: T) {
+        while let Some(last) = self.stack.last()
+            && last < &val
+        {
+            self.stack.pop();
+        }
+
+        self.stack.push(val);
+    }
+
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use spectral::prelude::*;
+
+    #[test]
+    fn test() {
+        use Operation::*;
+        let mut stack = MonotonicDescStack { stack: vec![] };
+
+        let operations = vec![
+            (Push(5), Some(vec![5])),
+            (Push(3), Some(vec![5, 3])),
+            (Push(7), Some(vec![7])),
+            (Push(2), Some(vec![7, 2])),
+            (Pop, Some(vec![7])),
+            (Pop, Some(vec![])),
+            (Pop, None),
+        ];
+
+        for (operation, expected_stack) in operations {
+            match operation {
+                Operation::Push(val) => {
+                    stack.push(val);
+                    if let Some(expected) = expected_stack {
+                        assert_that!(&stack.stack).is_equal_to(&expected);
+                    }
+                }
+                Operation::Pop => {
+                    let result = stack.pop();
+                    if let Some(expected) = expected_stack {
+                        assert_that!(&stack.stack).is_equal_to(&expected);
+                    } else {
+                        assert_that!(result).is_none();
+                    }
+                }
+            }
+        }
+    }
+
+    enum Operation {
+        Push(i32),
+        Pop,
+    }
+}
