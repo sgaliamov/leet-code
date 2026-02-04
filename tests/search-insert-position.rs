b552b668bd9e1ca4a66c2277be7abe93f35f0286
s. galiamov <6alyamov@gmail.com>
2026-02-04 23:10:23 +0100

    search_insert_2

--- tests/search-insert-position.rs
+++ tests/search-insert-position.rs
@@ -3,8 +3,23 @@
 //! Given a sorted array of distinct integers and a target value, return the index if the target is found.
 //! If not, return the index where it would be if it were inserted in order.
 //! You must write an algorithm with O(log n) runtime complexity.
+//!
+//! Constraints:
+//! - 1 <= nums.length <= 10^4
+//! - -10^4 <= nums[i] <= 10^4
+//! - nums contains distinct values sorted in ascending order.
+//! - -10^4 <= target <= 10^4
 
+// 0ms | 2.20MB - 34%
 pub fn search_insert_1(nums: Vec<i32>, target: i32) -> i32 {
+    match nums.binary_search(&target) {
+        Ok(i) => i as i32,
+        Err(i) => i as i32,
+    }
+}
+
+// 0ms | 2.20MB - 84.75%
+pub fn search_insert_2(nums: Vec<i32>, target: i32) -> i32 {
     match nums.binary_search(&target) {
         Ok(i) => i as i32,
         Err(i) => i as i32,
