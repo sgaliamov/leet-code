4bb7b10dc53efefa0ead76c4c7fbf17cf5a41ce7
s. galiamov <6alyamov@gmail.com>
2026-02-04 21:19:57 +0100

    final_prices_2 draft

--- tests/final-prices-with-a-special-discount-in-a-shop.rs
+++ tests/final-prices-with-a-special-discount-in-a-shop.rs
@@ -10,10 +10,10 @@
 //! of the shop, considering the special discount.
 //!
 //! Constraints:
-//! - `1 <= prices.length <= 500`
-//! - `1 <= prices[i] <= 1000`
+//! - 1 <= prices.length <= 500
+//! - 1 <= prices[i] <= 1000
 
-use leet_code::push_decreasing;
+use leet_code::{push_decreasing, push_increasing};
 
 // 0ms | 2.07MB - 100% | O(n²)
 pub fn final_prices_1(mut prices: Vec<i32>) -> Vec<i32> {
@@ -21,6 +21,41 @@ pub fn final_prices_1(mut prices: Vec<i32>) -> Vec<i32> {
         let p = prices[i];
         let d = *prices.iter().skip(i + 1).find(|&x| x <= &p).unwrap_or(&0);
         prices[i] -= d;
+    }
+
+    prices
+}
+
+pub fn final_prices_2(mut prices: Vec<i32>) -> Vec<i32> {
+    use std::collections::VecDeque;
+
+    fn push_back_decreasing<T: PartialOrd>(stack: &mut VecDeque<T>, val: T) {
+        while let Some(last) = stack.back()
+            && last < &val
+        {
+            stack.pop_back();
+        }
+        stack.push_back(val);
+    }
+
+    let mut stack = VecDeque::new();
+
+    for i in 0..prices.len() - 1 {
+        let price = prices[i];
+        let next = prices[i + 1];
+
+        if next <= price {
+            prices[i] = price - next;
+            stack.pop_front();
+        } else {
+            for j in i + 1..prices.len() {
+                push_back_decreasing(&mut stack, prices[j]);
+                if prices[j] <= price {
+                    prices[i] = price - prices[j];
+                    break;
+                }
+            }
+        }
     }
 
     prices
@@ -35,13 +70,14 @@ mod tests {
     solution_tests!(
         run_test:
         final_prices_1,
+        final_prices_2
     );
 
     fn run_test(target: fn(Vec<i32>) -> Vec<i32>) {
         vec![
-            (vec![8, 4, 6, 2, 3], vec![4, 2, 4, 2, 3]), // Example 1: discounts applied
-            (vec![1, 2, 3, 4, 5], vec![1, 2, 3, 4, 5]), // Example 2: no discounts
-            (vec![10, 1, 1, 6], vec![9, 0, 1, 6]),      // Example 3: mixed discounts
+            (vec![8, 10, 9, 4, 6, 6, 2, 3], vec![4, 1, 5, 2, 0, 4, 2, 3]),
+            (vec![1, 2, 3, 4, 5], vec![1, 2, 3, 4, 5]),
+            (vec![10, 1, 1, 6], vec![9, 0, 1, 6]),
         ]
         .into_iter()
         .for_each(|(prices, expected)| {
