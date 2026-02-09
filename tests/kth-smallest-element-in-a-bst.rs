5e50fc61247bb8834543b695474c624575c6fe8c
s. galiamov <6alyamov@gmail.com>
2026-02-09 21:25:25 +0100

    wrong

--- tests/kth-smallest-element-in-a-bst.rs
+++ tests/kth-smallest-element-in-a-bst.rs
@@ -15,26 +15,8 @@ use leet_code::TreeNode;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-// wrong
+// 0ms | 3MB - 98.04% - wrong
 pub fn kth_smallest_1(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
-    let mut node = root.unwrap();
-    let mut path = vec![node.clone()];
-
-    loop {
-        let Some(left) = node.borrow_mut().left.take() else {
-            break;
-        };
-
-        path.push(left.clone());
-        node = left;
-    }
-
-    let k = path.len().min(k as usize);
-    path[path.len() - k as usize].borrow().val
-}
-
-// 0ms | 3MB - 98.04%
-pub fn kth_smallest_2(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
     use std::collections::VecDeque;
 
     let mut stack = VecDeque::new();
@@ -58,6 +40,47 @@ pub fn kth_smallest_2(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
     values[k as usize - 1]
 }
 
+pub fn kth_smallest_2(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+    let mut stack = Vec::new();
+    let mut values = vec![];
+    let node = root.unwrap();
+    stack.push(node);
+
+    while let Some(node) = stack.pop() {
+        if let Some(right) = node.borrow_mut().right.take() {
+            stack.push(right);
+        };
+
+        if let Some(left) = node.borrow_mut().left.take() {
+            stack.push(left);
+        };
+
+        values.push(node.borrow().val);
+    }
+
+    values[k as usize - 1]
+}
+
+pub fn kth_smallest_3(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+    fn traverse(node: Option<Rc<RefCell<TreeNode>>>, values: &mut Vec<i32>) -> Option<i32> {
+        let node = node?;
+
+        if let Some(l) = traverse(node.borrow_mut().left.take(), values) {
+            values.push(l);
+        }
+
+        if let Some(r) = traverse(node.borrow_mut().right.take(), values) {
+            values.push(r);
+        }
+
+        Some(node.borrow().val)
+    }
+
+    let mut values = vec![];
+    traverse(root, &mut values);
+    values[k as usize - 1]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,10 +91,12 @@ mod tests {
         run_test:
         kth_smallest_1,
         kth_smallest_2,
+        kth_smallest_3,
     );
 
     fn run_test(target: fn(Option<Rc<RefCell<TreeNode>>>, i32) -> i32) {
         vec![
+            (vec![Some(1), Some(2), Some(3)], 3, 3),
             (vec![Some(1), None, Some(2)], 2, 2),
             (vec![Some(3), Some(1), Some(4), None, Some(2)], 2, 2),
             (vec![Some(1)], 1, 1),
