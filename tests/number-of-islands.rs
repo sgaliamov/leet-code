9c14170fe9748b031564791b4e3c78d017f58df7
s. galiamov <6alyamov@gmail.com>
2026-02-18 23:45:41 +0100

    failed

--- tests/number-of-islands.rs
+++ tests/number-of-islands.rs
@@ -12,8 +12,45 @@
 //! - 1 <= m, n <= 300
 //! - grid[i][j] is '0' or '1'
 
-pub fn num_islands_1(grid: Vec<Vec<char>>) -> i32 {
-    todo!("solve")
+pub fn num_islands_1(mut grid: Vec<Vec<char>>) -> i32 {
+    let mut cnt = b'1';
+
+    let m = grid.len() - 1;
+    for i in 0..=m {
+        let n = grid[i].len() - 1;
+
+        for j in 0..=n {
+            if grid[i][j] == '0' || grid[i][j] != '1' {
+                continue;
+            }
+
+            let lf = if j != 0 { grid[i][j - 1] } else { '0' };
+            let rt = if j != n { grid[i][j + 1] } else { '0' };
+            let up = if i != 0 { grid[i - 1][j] } else { '0' };
+            let dw = if i != m { grid[i + 1][j] } else { '0' };
+
+            let mut cur = up.max(dw).max(lf).max(rt).max(grid[i][j]);
+
+            if cur == '1' {
+                cnt += 1;
+                cur = cnt as char;
+            }
+
+            grid[i][j] = cur;
+
+            if up != '0' {
+                grid[i - 1][j] = cur;
+            }
+            if dw != '0' {
+                grid[i + 1][j] = cur;
+            }
+            if rt != '0' {
+                grid[i][j + 1] = cur;
+            }
+        }
+    }
+
+    (cnt - b'1') as i32
 }
 
 #[cfg(test)]
@@ -31,13 +68,29 @@ mod tests {
         vec![
             (
                 vec![
+                    vec!['1', '0', '1', '1', '1'],
+                    vec!['1', '0', '1', '0', '1'],
+                    vec!['1', '1', '1', '0', '1'],
+                ],
+                1,
+            ),
+            (
+                vec![
+                    vec!['1', '1', '1'],
+                    vec!['0', '1', '0'],
+                    vec!['1', '1', '1'],
+                ],
+                1,
+            ),
+            (
+                vec![
                     vec!['1', '1', '1', '1', '0'],
                     vec!['1', '1', '0', '1', '0'],
                     vec!['1', '1', '0', '0', '0'],
                     vec!['0', '0', '0', '0', '0'],
                 ],
                 1,
-            ), // Example 1: single large island
+            ),
             (
                 vec![
                     vec!['1', '1', '0', '0', '0'],
@@ -46,11 +99,21 @@ mod tests {
                     vec!['0', '0', '0', '1', '1'],
                 ],
                 3,
-            ), // Example 2: three separate islands
+            ),
+            (
+                vec![
+                    vec!['0', '0', '0', '0', '0'],
+                    vec!['0', '0', '0', '0', '0'],
+                    vec!['0', '0', '0', '0', '0'],
+                    vec!['0', '0', '0', '0', '0'],
+                ],
+                0,
+            ),
         ]
         .into_iter()
-        .for_each(|(grid, expected)| {
-            let name = format!("{}x{} grid", grid.len(), grid[0].len());
+        .enumerate()
+        .for_each(|(i, (grid, expected))| {
+            let name = format!("{}x{} grid {}", grid.len(), grid[0].len(), i + 1);
             let actual = target(grid);
             assert_that!(actual).named(&name).is_equal_to(expected);
         });
