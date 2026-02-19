fc0c251dc1de1c75a1754c239e374b000a36cbe1
s. galiamov <6alyamov@gmail.com>
2026-02-19 23:52:19 +0100

    bench

--- benches/contains-duplicate-ii.rs
+++ benches/contains-duplicate-ii.rs
@@ -43,6 +43,29 @@ fn with_default_hasher(nums: Vec<i32>, k: i32) -> bool {
     false
 }
 
+// no benefits
+fn with_unsafe(nums: Vec<i32>, k: i32) -> bool {
+    let k = k as usize;
+    let mut set = HashSet::with_hasher(BuildHasherDefault::<DefaultHasher>::default());
+
+    for i in 0..nums.len() {
+        unsafe {
+            let n = *nums.get_unchecked(i);
+
+            if !set.insert(n) {
+                return true;
+            }
+
+            let remove_idx = i.wrapping_sub(k);
+            if remove_idx < i && remove_idx < nums.len() {
+                set.remove(nums.get_unchecked(remove_idx));
+            }
+        }
+    }
+
+    false
+}
+
 fn benchmark_hashers(c: &mut Criterion) {
     let mut group = c.benchmark_group("hasher_comparison");
 
@@ -58,6 +81,10 @@ fn benchmark_hashers(c: &mut Criterion) {
         b.iter(|| with_default_hasher(black_box(small_input.clone()), black_box(k)))
     });
 
+    group.bench_function("unsafe_small", |b| {
+        b.iter(|| with_unsafe(black_box(small_input.clone()), black_box(k)))
+    });
+
     // Large window, no duplicates found
     let large_input = (1..=10000).collect::<Vec<_>>();
     let k = 1000;
@@ -68,6 +95,10 @@ fn benchmark_hashers(c: &mut Criterion) {
 
     group.bench_function("default_hasher_large", |b| {
         b.iter(|| with_default_hasher(black_box(large_input.clone()), black_box(k)))
+    });
+
+    group.bench_function("unsafe_large", |b| {
+        b.iter(|| with_unsafe(black_box(large_input.clone()), black_box(k)))
     });
 
     group.finish();
