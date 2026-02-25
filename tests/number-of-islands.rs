3d9d7504fcc06feded9b2f4ff9482d6953bf70d7
s. galiamov <6alyamov@gmail.com>
2026-02-25 22:16:13 +0100

    num_islands_1

--- tests/number-of-islands.rs
+++ tests/number-of-islands.rs
@@ -12,46 +12,95 @@
 //! - 1 <= m, n <= 300
 //! - grid[i][j] is '0' or '1'
 
-// todo: solve
 pub fn num_islands_1(mut grid: Vec<Vec<char>>) -> i32 {
-    let mut cnt = b'1';
+    let mut marker = 2_u8;
+    let mut cnt = 0;
+    let mut map = std::collections::HashMap::new();
+    let mut active = false;
 
     let m = grid.len() - 1;
     for i in 0..=m {
         let n = grid[i].len() - 1;
 
         for j in 0..=n {
-            if grid[i][j] == '0' || grid[i][j] != '1' {
+            if grid[i][j] == '0' {
+                if active {
+                    marker += 1;
+                    active = false;
+                }
                 continue;
+            } else {
+                active = true;
             }
 
-            let lf = if j != 0 { grid[i][j - 1] } else { '0' };
-            let rt = if j != n { grid[i][j + 1] } else { '0' };
             let up = if i != 0 { grid[i - 1][j] } else { '0' };
-            let dw = if i != m { grid[i + 1][j] } else { '0' };
-
-            let mut cur = up.max(dw).max(lf).max(rt).max(grid[i][j]);
-
-            if cur == '1' {
-                cnt += 1;
-                cur = cnt as char;
+            if up == '0' {
+                grid[i][j] = marker as char;
+                map.entry(grid[i][j]).or_insert_with(|| {
+                    cnt += 1;
+                    '0'
+                });
+            } else {
+                let c = marker as char;
+                match map.get_mut(&c) {
+                    Some(known) => {
+                        if known != &up {
+                            *known = up;
+                            cnt -= 1;
+                        }
+                    }
+                    None => {
+                        map.entry(c).or_insert_with(|| {
+                            // cnt -= 1;
+                            up
+                        });
+                    }
+                }
             }
+        }
 
-            grid[i][j] = cur;
+        active = false;
+        marker += 1;
+    }
 
-            if up != '0' {
-                grid[i - 1][j] = cur;
-            }
-            if dw != '0' {
-                grid[i + 1][j] = cur;
-            }
-            if rt != '0' {
-                grid[i][j + 1] = cur;
+    cnt
+}
+
+pub fn num_islands_2(mut grid: Vec<Vec<char>>) -> i32 {
+    fn explore(i: usize, j: usize, grid: &mut Vec<Vec<char>>) {
+        let m = grid.len() - 1;
+        for i in i..=m {
+            let n = grid[i].len() - 1;
+
+            for j in 0..=n {
+                let c = grid[i][j] as u8 - b'1';
+
+                let lf = if j != 0 { grid[i][j - 1] } else { '0' };
+                let rt = if j != n { grid[i][j + 1] } else { '0' };
+                let up = if i != 0 { grid[i - 1][j] } else { '0' };
+                let dw = if i != m { grid[i + 1][j] } else { '0' };
+
+                if c == 0 {
+                    continue;
+                }
             }
         }
     }
 
-    (cnt - b'1') as i32
+    let mut cnt = b'1';
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            let c = grid[i][j] as u8 - b'1';
+            if c != 1 {
+                continue;
+            }
+
+            explore(i, j, &mut grid);
+        }
+    }
+
+    0
 }
 
 #[cfg(test)]
@@ -67,6 +116,44 @@ mod tests {
 
     fn run_test(target: fn(Vec<Vec<char>>) -> i32) {
         vec![
+            (
+                vec![
+                    vec!['1', '0', '1'],
+                    vec!['1', '0', '1'],
+                    vec!['1', '0', '1'],
+                ],
+                2,
+            ),
+            (
+                vec![
+                    vec!['1', '1', '0', '1', '1'], //
+                    vec!['0', '1', '1', '1', '1'],
+                ],
+                1,
+            ),
+            (
+                vec![
+                    vec!['0', '0', '1', '1'], //
+                    vec!['1', '1', '1', '0'],
+                ],
+                1,
+            ),
+            (
+                vec![
+                    vec!['1', '1'], //
+                    vec!['1', '1'],
+                ],
+                1,
+            ),
+            (vec![vec!['1']], 1),
+            (
+                vec![
+                    vec!['1', '0', '1', '0', '1'],
+                    vec!['1', '0', '1', '0', '1'],
+                    vec!['1', '1', '1', '1', '1'],
+                ],
+                1,
+            ),
             (
                 vec![
                     vec!['1', '1', '0', '0', '0'],
